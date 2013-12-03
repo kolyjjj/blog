0d9d70854c6915c50b375f5cf3b747ee6de80332
kolyjjj <me.koly.li@gmail.com>
2013-12-03 11:27:15 +0800

    add posts controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Myapp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get 'welcome/index'
 
+  resources :posts
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
